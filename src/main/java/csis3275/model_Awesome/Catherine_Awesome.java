9892840a680c4c2a32c7eaa3485fabe602ad8d12
camm9 <student.camm@gmail.com>
2023-09-30 16:31:35 -0700

    Feat: added to catherine selfie class and added photo to /static

---
+++ src/main/java/csis3275/model_Awesome/Catherine_Awesome.java
@@ -0,0 +1,17 @@
+package csis3275.model_Awesome;
+
+public class Catherine_Awesome extends Person {
+	
+	
+	
+	Catherine_Awesome(){
+		
+	}
+	
+	Catherine_Awesome(String bio, String image){
+		this.bio = bio;
+		this.image = image;
+		
+	}
+
+}
--- src/main/java/csis3275/model_Awesome/Catherine_selfie.java
+++
@@ -1,5 +0,0 @@
-package csis3275.model_Awesome;
-
-public class Catherine_selfie extends Person {
-
-}
