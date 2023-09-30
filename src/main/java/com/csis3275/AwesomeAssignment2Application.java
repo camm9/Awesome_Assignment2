26c4a457e9385640f2e1d5d1d3f141269d19dfae
camm9 <student.camm@gmail.com>
2023-09-30 12:42:48 -0700

    first commit

---
+++ src/main/java/com/csis3275/AwesomeAssignment2Application.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeAssignment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeAssignment2Application.class, args);
+	}
+
+}
