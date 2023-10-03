0c28678c1f742bcc2a94bf80d1481be0a7969085
Washington Valencia <valenciaasquiw@student.douglascollege.ca>
2023-10-02 17:03:22 -0700

    fix: Added new method to render profile page

--- src/main/java/com/csis3275/controller_Awesome/Controller_Awesome.java
+++ src/main/java/com/csis3275/controller_Awesome/Controller_Awesome.java
@@ -1,6 +1,7 @@
 package com.csis3275.controller_Awesome;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -13,5 +14,19 @@ public class Controller_Awesome {
 	@GetMapping("/")
 	public String renderHome() {
 		return "home";
+	}
+	
+	
+	@GetMapping("/wva-06")
+	public String renderProfilePage(Model model) {
+			
+		model.addAttribute("name", "Washington Valencia");
+		model.addAttribute("bio", "Entrepreneur, passionate, enthusiastic with high knowledge of IT and good leadership skills. In continuous professional training. Always adaptable and with all the desire to grow and learn.");
+		model.addAttribute("image", "assets/images/images/wva-06.jpg");
+		model.addAttribute("rollName", "Dev/Ops");
+		model.addAttribute("indeedUrl", "https://www.linkedin.com/in/washington-eduardo-valencia-1ab8aa189/");
+		model.addAttribute("githubUrl", "https://github.com/educristo777");
+	
+		return "wva-06";
 	}
 }
